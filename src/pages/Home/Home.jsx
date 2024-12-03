e662c5a882af950ebf56b25bf6a6ef6cfb6f6e34
Dawit <116512754+Dawa-22@users.noreply.github.com>
2024-12-03 17:29:44 +0300

    Describe your changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,8 +17,7 @@ const Home = () => {
         <div className="hero-caption">
           <img src={hero_title} alt="" className="caption-img" />
           <p>
-            blalallalllalalalalallbllllll vllllllllllnellelelelelelelelelelelle
-            lelelelelelelelelelllllllllllllllllllllllllllll.
+            Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.
           </p>
           <div className="hero-btns">
             <button className="btn">
